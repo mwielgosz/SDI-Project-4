4ee15f9550d76d916678b55c60f8b704630c1dfb
Mike Wielgosz <dev.mwielgosz@gmail.coom>
2014-02-23 22:26:19 -0500

    Setup bare functions

--- wielgosz-michael-project4.js
+++ wielgosz-michael-project4.js
@@ -4,3 +4,96 @@
 // SDI 1402
 // Project 4
 // Library Functions
+
+
+//String Functions
+
+
+// Function to determine if String is in telephons number format: "123-456-789"
+var isTelephoneNumber = function(input) {
+    var patternCorrect = false;
+    
+    return patternCorrect;
+}
+
+// Function to determine is String is in email address format: "email@address.com"
+var isEmailAddress = function(input) {
+    var patternCorrect = false;
+    
+    return patternCorrect;
+}
+
+// Function to determine if String is in URL format: "http://" or "https://"
+var isURL = function(input) {
+    var patternCorrect = false;
+    
+    return patternCorrect;
+}
+
+// Function to capitalize each word in String
+var capitalizeString = function(input) {
+    var finalString = "";
+    
+    return finalString;
+}
+
+// Function to replace String seperators: "a,b,c" "a/b/c"
+var refactorStringWithReplacement = function(string, initialSeperator, returnSeperator) {
+    var finalString = ""
+    
+    return finalString;
+}
+
+
+// Number Functions
+
+// Function to return Float with X number of decimal places
+var refactorNumberDecimals = function(number, numDecimals) {
+    var returnFloat = 0.0;
+    
+    return returnFloat;
+}
+
+// Function to determine if first number is greater than second and if third is within the first and second
+var percentageFuzzyMatch = function(firstNum, secondNum, thirdNum) {
+    
+}
+
+
+// Function to get the difference in time and return values in hours or days
+var getTimeDifference = function(firstData, secondData, hoursOrDays) {
+    var returnTime = "";
+    
+    return returnTime;
+}
+
+// Function to return a String number as Number
+var getStringAsInteger = function(stringNumber) {
+    var isNumber = false,
+        number = 0;
+        
+    return number;
+}
+
+// Array Functions
+
+// Function to get the next largest number in an array as determined by the input
+var getHighestArrayNumber = function(numArray, highestNum) {
+    var returnNumber = 0;
+    
+    return returnNumber;
+}
+
+// Function to get the total number of Numbers in a given array
+var getTotalNumbersInArray = function(array) {
+    var totalNumbers = 0;
+    
+    return totalNumbers;
+}
+
+// Function to return the Objects in an array determined by a given key
+var getObjectsByKey = function(objectAray, key) {
+    var returnArray = [];
+
+    return returnArray;
+}
